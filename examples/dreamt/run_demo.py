b8927cc9749a7c4e69435d8eeb45c567240e6e4e
Yacine Belal <yacine.belal@cea.fr>
2026-02-27 15:00:20 +0100

    remove absolute path to dreamt dataset

--- examples/dreamt/run_demo.py
+++ examples/dreamt/run_demo.py
@@ -7,7 +7,7 @@ from declearn.test_utils import make_importable
 from declearn.utils import run_as_processes
 
 with make_importable(os.path.dirname(__file__)):
-    # from prepare_data import prepare_dreamt
+    from prepare_data import prepare_dreamt
     from run_client import run_client
     from run_server import run_server
 
@@ -16,10 +16,7 @@ def run_demo(
     nb_clients: int =20, 
     seed: Optional[int] = None,
 ) -> None:
-    
-    data_folder = "/volatile/home/yb285618/SSF/declearn/examples/dreamt/data/dreamt_natural"
-    #prepare_dreamt(nb_clients, seed=seed)
-    # "/volatile/home/yb285618/SSF/declearn/examples/dreamt/data/dreamt_natural"
+    data_folder = prepare_dreamt(nb_clients, seed=seed)
     server = (run_server, {"nb_clients": nb_clients})
     client_kwargs = {
         "data_folder" : data_folder,
