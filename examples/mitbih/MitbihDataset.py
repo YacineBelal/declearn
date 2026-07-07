0a5a0b58d24852577c6aab878291c44fc2eebf8b
Yacine Belal <yacine.belal@cea.fr>
2026-07-07 11:37:54 +0200

    Update mitbih data model to compute and feed derivative of the signal to the model

--- examples/mitbih/MitbihDataset.py
+++ examples/mitbih/MitbihDataset.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import Dataset
 
 
@@ -5,17 +6,24 @@ class MitbihDataset(Dataset):
     """Dataset for MIT-BIH data with RR intervals features.
 
     Expects an X in (C, T) format 4 RR feature.
-    Returns (X, RR, y) tuples.
+    Returns (X_deriv, RR, y) tuples.
     """
 
-    def __init__(self, X, RR, y):
+    # TODO: add attribute to control whether to return deriv or X or both
+    def __init__(self, X, RR, y, get_deriv=True):
         super().__init__()
-        self.x = X
-        self.rr = RR
-        self.y = y
+        self.x = torch.as_tensor(X, dtype=torch.float32)
+        self.rr = torch.as_tensor(RR, dtype=torch.float32)
+        self.y = torch.as_tensor(y, dtype=torch.long)
+        self.deriv_x = torch.diff(self.x, dim=-1, prepend=self.x[..., :1])
+        self.get_deriv = get_deriv
 
     def __getitem__(self, index):
-        return [self.x[index], self.rr[index]], self.y[index]
+        return (
+            self.deriv_x[index] if self.get_deriv else self.x[index],
+            self.rr[index],
+            self.y[index],
+        )
 
     def __len__(self):
         return len(self.y)
