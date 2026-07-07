483f16aaf48cf88c6dcc06aeae0309498b6c0654
Yacine Belal <yacine.belal@cea.fr>
2026-07-07 14:19:06 +0200

    Add tinyCNN model architecture

--- examples/mitbih/model.py
+++ examples/mitbih/model.py
@@ -2,6 +2,68 @@ import torch
 from torch import nn
 
 
+class tinyCNN(nn.Module):
+    """Frugal Model based on Matched Filters proposed by (Farag et al., 23)
+    for edge ECG-based arrhythmia detection.
+
+    Parameters
+    ----------
+    matched_filters : Tensor of shape (output_channels, 1, window_len)
+        represents the average train signals windowed by subclass. Hence, output_channels
+        is the number of subclasses considered. For eg., for MIT-BIH, its equal to 11.
+
+    trainable_conv : Boolean
+        whether to train the conv layer initialized with the matched_filters.
+    """
+
+    def __init__(self, matched_filters, trainable_conv=True):
+        super().__init__()
+        mf = torch.as_tensor(matched_filters, dtype=torch.float32)
+        if not trainable_conv:
+            self.register_buffer("mf", mf)
+        else:
+            self.mf = nn.Parameter(mf.clone())
+
+        n_filters = mf.shape[0]
+
+        self.post_convolution = nn.Sequential(
+            nn.BatchNorm1d(n_filters),
+            nn.Tanh(),
+            nn.AdaptiveMaxPool1d(1),
+        )
+
+        self.rr_path = nn.Sequential(
+            nn.Linear(4, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+        )
+
+        self.merger = nn.Sequential(nn.Linear(n_filters + 8, 3))
+
+        self._init_weights()
+
+    def _init_weights(self):
+        for layer in self.modules():
+            if isinstance(layer, nn.Linear):
+                nn.init.kaiming_normal_(layer.weight)
+                nn.init.zeros_(layer.bias)
+            elif isinstance(layer, nn.BatchNorm1d):
+                nn.init.ones_(layer.weight)
+                nn.init.zeros_(layer.bias)
+
+    def forward(self, X, rr):
+        conv_out = nn.functional.conv1d(
+            input=X, weight=self.mf, padding="same"
+        )
+        conv_out = self.post_convolution(conv_out)
+        rr_path_out = self.rr_path(rr)
+        return self.merger(
+            torch.cat([conv_out.squeeze(-1), rr_path_out], dim=1)
+        )
+
+
 class CNN(nn.Module):
     """simple CNN with a multihead attention layer for the MIT-BIH arrhythmia detection task
 
